7a9b7be764797f20f1e1a768136c18537246c58d
JoshDowling <jk2md@live.co.uk>
2019-01-28 17:15:24 +0000

    Added account name to account and Db

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO ACCOUNT (accountid, username, password) VALUES (1, 'JohnDoe', 'password');
+INSERT INTO ACCOUNT (accountid, name, username, password) VALUES (1, 'John Doe','JohnDoe1', 'password');
 INSERT INTO TEAM (teamid, teamname, accountid) VALUES (1, 'testeam', 1);
